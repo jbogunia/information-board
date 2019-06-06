2f6cdd65b7bedaf3bfbad4d178b599ca736db7fb
Radoslaw Skrodzki <rad.skrodzki@gmail.com>
2019-06-06 11:34:44 +0200

    Fix ErrorResponse class

--- class/ErrorResponse.cpp
+++ class/ErrorResponse.cpp
@@ -3,5 +3,23 @@ private:
     int code;
     String message;
 public:
+	ErrorResponse(int code){
+		this -> code = code;
+	}
+	ErrorResponse(int code, String message){
+		this -> code = code;
+		this -> message = message;
+	}
 
+	int getCode(){
+		return code;
+	}
+
+	const String getMessage(){
+		return message;
+	}
+
+	const String getJsonMessage(){
+		return "{\"message\" : \"" + this -> getMessage() + "\"}";
+	}
 };
