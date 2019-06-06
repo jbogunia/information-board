611434f28dd240eb23d491e85fb910b5c1bac9df
Jakub <bogunia.jakub@gmail.com>
2019-06-06 10:08:08 +0200

    Hadnle save adverts to .json file (Adverts backup)

--- class/FileAdapter.cpp
+++ class/FileAdapter.cpp
@@ -4,11 +4,11 @@
 
 class FileAdapter {
 
-private:
+  private:
     FileAdapter();
     bool saveAdvertsToJson();
 
-public:
+  public:
     JsonArray loadAdverts();
     ErrorResponse saveAdvert(JsonObject newAdvert);
     ErrorResponse removeAdvert(int id, String advertPassword);
@@ -19,14 +19,14 @@ public:
     StaticJsonDocument<2000> arrayDoc;
     JsonObject rootObject;
     JsonArray advertsArray;
-	  ~FileAdapter();
+    ~FileAdapter();
 };
 
 FileAdapter::FileAdapter () {
-    SPIFFS.begin();
-    Serial.println("File adapter initialized ... Ready");
-    this->rootObject = this->arrayDoc.to<JsonObject>();
-    this->advertsArray = this->rootObject.createNestedArray("adverts");
+  SPIFFS.begin();
+  Serial.println("File adapter initialized ... Ready");
+  this->rootObject = this->arrayDoc.to<JsonObject>();
+  this->advertsArray = this->rootObject.createNestedArray("adverts");
 };
 
 FileAdapter& FileAdapter::getInstance() {
@@ -35,29 +35,71 @@ FileAdapter& FileAdapter::getInstance() {
 }
 
 FileAdapter::~FileAdapter() {
-    this->arrayDoc.clear();
+  this->arrayDoc.clear();
 }
 
 JsonArray FileAdapter::loadAdverts() {
+  File adverts = SPIFFS.open("/adverts.json", "r");
+  if (!adverts) {
+    Serial.println("Reading adverts file failure...");
+    return this->advertsArray;
+  }
 
+  adverts.setTimeout(5000);
+
+  size_t size = adverts.size();
+  Serial.print("Adverts file size: ");
+  Serial.println(size);
+  if (size == 0) {
+    Serial.println("Adverts file is empty!");
+    return this->advertsArray;
+  }
+
+  DynamicJsonDocument doc(400);
+
+  adverts.find("\"adverts\":[");
+  do {
+    DeserializationError err = deserializeJson(doc, adverts);
+    if (err) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(err.c_str());
+    }
+    JsonObject object = doc.as<JsonObject>();
+    this->advertsArray.add(object);
+  } while (adverts.findUntil(",", "]"));
+  serializeJsonPretty(this->rootObject, Serial);
+  adverts.close();
+  return this->advertsArray;
 }
 
-bool FileAdapter::saveAdvertsToJson(){
+bool FileAdapter::saveAdvertsToJson() {
+  File advertsFile = SPIFFS.open("/adverts.json", "w");
+  if (!advertsFile) {
+    Serial.println("Problem with opening adverts.json file");
+    return false;
+  }
 
+  Serial.println("");
+  Serial.println("Saving adverts to json file...");
+
+  serializeJson(this->rootObject, advertsFile);
+  serializeJsonPretty(this->rootObject, Serial);
+  advertsFile.close();
+  return true;
 }
 
 ErrorResponse FileAdapter::saveAdvert(JsonObject newAdvert) {
 
 }
 
-int FileAdapter::getAdvertIndex(int advertId){
+int FileAdapter::getAdvertIndex(int advertId) {
 
 }
 
-ErrorResponse FileAdapter::removeAdvert(int id, String advertPassword){
-  
+ErrorResponse FileAdapter::removeAdvert(int id, String advertPassword) {
+
 }
 
-ErrorResponse FileAdapter::editAdvert(int id, String title, String body, String password){
-  
+ErrorResponse FileAdapter::editAdvert(int id, String title, String body, String password) {
+
 }
