455efe8995d825133fd44a72c58dcb483b073f1e
johnynov <j.wieczorek34@gmail.com>
2019-06-06 11:47:02 +0200

    Added HTTP_DELETE in information-board.ino and "removeAdvert" in FileAdapter.cpp Fixed line indent.

--- information-board.ino
+++ information-board.ino
@@ -7,75 +7,91 @@ AsyncWebServer server(80);
 
 void setup() {
 
-  Serial.begin(115200);
-  Serial.println();
+    Serial.begin(115200);
+    Serial.println();
 
-  JsonArray adverts = fileAdapter.loadAdverts();
+    JsonArray adverts = fileAdapter.loadAdverts();
 
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_AP);
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_AP);
 
-  Serial.print("Setting soft-AP configuration ... ");
-  Serial.println(WiFi.softAPConfig(localIp, gateway, subnet) ? "Ready" : "Failed!");
+    Serial.print("Setting soft-AP configuration ... ");
+    Serial.println(WiFi.softAPConfig(localIp, gateway, subnet) ? "Ready" : "Failed!");
 
-  Serial.print("Setting soft-AP ... ");
-  Serial.println(WiFi.softAP("Information-Board") ? "Ready" : "Failed!");
+    Serial.print("Setting soft-AP ... ");
+    Serial.println(WiFi.softAP("Information-Board") ? "Ready" : "Failed!");
 
-  Serial.print("Soft-AP IP address = ");
-  Serial.println(WiFi.softAPIP());
+    Serial.print("Soft-AP IP address = ");
+    Serial.println(WiFi.softAPIP());
 
-  // Initialize SPIFFS
-  if (!SPIFFS.begin()) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
+    // Initialize SPIFFS
+    if (!SPIFFS.begin()) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
 
-  //Tutaj obsługiwane są requuesty, poniższy zwraca plik index.html z pamięci urządzenia
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/index.html");
-    Serial.println("Index.html GET Request");
-  });
+    //Tutaj obsługiwane są requuesty, poniższy zwraca plik index.html z pamięci urządzenia
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+        request->send(SPIFFS, "/index.html");
+        Serial.println("Index.html GET Request");
+    });
 
-  //Ten endpoint odpowiada za zwrócenie listy wszystkich ogłoszeń w formacie JSON
-  //(wraca z pamięci podręcznej, ale wszystkie ogłoszenia zapisywane/modyfikowane są równiez w pliku adverts.json - dostępne są po restartie urządzenia)
-  server.on("/advert", HTTP_GET, [](AsyncWebServerRequest * request) {
+    //Ten endpoint odpowiada za zwrócenie listy wszystkich ogłoszeń w formacie JSON
+    //(wraca z pamięci podręcznej, ale wszystkie ogłoszenia zapisywane/modyfikowane są równiez w pliku adverts.json - dostępne są po restartie urządzenia)
+    server.on("/advert", HTTP_GET, [](AsyncWebServerRequest * request) {
+
+    });
+
+    //Zwraca kaskadowy arkusz stylów z pamięci urządzenia
+    server.on("/main.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+
+    });
+
+    //Zwraca js idk co to xD (pytać Karola)
+    server.on("/resources.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+
+    });
+
+    //Zwraca js, zapewne logika frontendu
+    server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+
+    });
+
+
+    //Endpoint umożliwiający dodanie nowego ogłoszenia
+    //Wymagane pola: title, body, password (trzeba by zrobić ich validacje, aktualnie tylko sprawdza, czy są w requescie)
+    server.on("/advert", HTTP_PUT, [](AsyncWebServerRequest * request) {
+
+    });
+
+    //Endpoint służący do edycji ogłoszenia
+    //Wymagane pola: id,title, body, password (rówież rzydała by się validacja)
+    server.on("/advert", HTTP_PATCH, [](AsyncWebServerRequest * request) {
+
+    });
+
+    //Endpoint służący do usuwania ogłoszenia
+    //Wymagane pola: id, password
+    server.on("/advert", HTTP_DELETE, [](AsyncWebServerRequest * request) {
+        Serial.println("Avdert remove request");
+        if (request->hasParam("id") && request->hasParam("password")) {
+            int advertId = request->getParam("id")->value().toInt();
+            String advertPassword = request->getParam("password")->value();
+            Serial.print("Removing advert: ");
+            Serial.println(advertId);
+
+            ErrorResponse errorResponse = fileAdapter.removeAdvert(advertId, advertPassword);
+            if (errorResponse.getCode() == 200) {
+                request->send(200, "application/json", errorResponse.getJsonMessage());
+            } else {
+                request->send(errorResponse.getCode(), "application/json", errorResponse.getJsonMessage());
+            }
+        } else {
+            request->send(400, "application/json", "{\"message\" : \"\You need to specyify all request params!\"}" );
+        }
+    });
     
-  });
-
-  //Zwraca kaskadowy arkusz stylów z pamięci urządzenia
-  server.on("/main.css", HTTP_GET, [](AsyncWebServerRequest * request) {
-   
-  });
-
-  //Zwraca js idk co to xD (pytać Karola)
-  server.on("/resources.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    
-  });
-
-  //Zwraca js, zapewne logika frontendu
-  server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-   
-  });
-
-
-  //Endpoint umożliwiający dodanie nowego ogłoszenia
-  //Wymagane pola: title, body, password (trzeba by zrobić ich validacje, aktualnie tylko sprawdza, czy są w requescie)
-  server.on("/advert", HTTP_PUT, [](AsyncWebServerRequest * request) {
-    
-  });
-
-  //Endpoint służący do edycji ogłoszenia
-  //Wymagane pola: id,title, body, password (rówież rzydała by się validacja)
-  server.on("/advert", HTTP_PATCH, [](AsyncWebServerRequest * request) {
-    
-  });
-
-  //Endpoint służący do usuwania ogłoszenia
-  //Wymagane pola: id, password
-  server.on("/advert", HTTP_DELETE, [](AsyncWebServerRequest * request) {
-    
-  });
-  server.begin();
+    server.begin();
 }
 
 // the loop function runs over and over again forever
